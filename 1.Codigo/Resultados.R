e358899f31f1e16a882b07d43a1b3bb54c483a3f
NAlcan <nalcann@gmail.com>
2020-12-04 16:43:31 -0300

    Primeros analisis

--- 1.Codigo/Resultados.R
+++ 1.Codigo/Resultados.R
@@ -1,22 +1,26 @@
-# Cargo las funciones -----------------------------------------------------
+# Funciones -----------------------------------------------------
 library(tidyverse) # Funciones de manipulacion de la base y graficos
-library(readxl) # para importar especificamente archivos .xlsx
-library(janitor)# algunas funciones mas para limpiar datos 
-# Cargo los datos de OAN --------------------------------------------------
-RN_RU <- read_excel("Datos 2009 - 2018 RN.RU.1.xlsx",
-                    .name_repair = make_clean_names  )
-
-# Un vistazo rapido. 315 Variables 1297 Observaciones
-#glimpse( RN_RU)
-
-      ## Limpieza de la Base ##
-# Hay caracteres especiales en columnas numericas ej:  
-lev_raros<-levels(factor(RN_RU$clorofila_a_mg_l))[c(1:5)]
 
 
-# Acomodos a la base:
-# 1. Eliminar valores especiales "< LD" (criterio conservador, se asumen NA)
-# 2. Editar comas por puntos para que tome valores numericos
-# 3. Seleccionar sitios y fehcas indicados en el articulo (primer parrafo de MyM y Tabla 1)
-# 4. Seleccionar variables usadas en el articulos
+# Datos -------------------------------------------------------------------
 
+data_articulo <- read_csv("2.Datos/data_articulo.csv")
+
+glimpse(data_articulo)
+
+# Voy a generar la variable "beret" que identifica los datos excluidos en articulos
+
+data_articulo <- data_articulo %>% 
+  mutate(beret = factor( ifelse ((codigo_pto == "RN12" & fecha_muestra == "2018-04-17"| 
+                            codigo_pto == "RN5" & fecha_muestra == "2012-01-19" |
+                            codigo_pto == "RN5" & fecha_muestra == "2012-12-06" |
+                            codigo_pto == "RN6" & fecha_muestra == "2012-08-15" |
+                            codigo_pto == "RN3" & fecha_muestra == "2012-12-05"  ), "out","inn")))
+
+ 
+ggplot(data_articulo,aes(y=clorofila_a_mg_l, x=nombre_programa)) +   geom_jitter(aes(color = beret)) + 
+  theme_classic()
+  
+ggplot(data_articulo, aes(y=clorofila_a_mg_l, x = pt_mg_p_l)) +
+  geom_point(aes(color = beret)) + facet_wrap( .~ nombre_programa) +
+  theme_classic()
