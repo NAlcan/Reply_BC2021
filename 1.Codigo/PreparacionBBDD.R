108283fd05945b28b5f795209614bfd1173b51d2
NAlcan <nalcann@gmail.com>
2020-12-04 16:14:47 -0300

    BBDD de articulo, version bereta y completa

---
+++ 1.Codigo/PreparacionBBDD.R
@@ -0,0 +1,109 @@
+# Cargo las funciones -----------------------------------------------------
+library(tidyverse) # Funciones de manipulacion de la base y graficos
+library(readxl) # para importar especificamente archivos .xlsx
+library(janitor)# algunas funciones mas para limpiar datos 
+library(lubridate) # para manipular obetos Fecha/Hora
+# Cargo los datos de OAN --------------------------------------------------
+RN_RU <- read_excel("Datos 2009 - 2018 RN.RU.1.xlsx",
+                    .name_repair = make_clean_names  ) # .name_repar = make_clean_names transforma los titulos de columnas todos uniformes
+
+# Un vistazo rapido. 315 Variables 1297 Observaciones
+glimpse( RN_RU)
+
+
+# Limpieza de la Base -----------------------------------------------------
+# Hay caracteres especiales en columnas numericas ej:  
+RN_RU %>%  dplyr::select (clorofila_a_mg_l) %>% 
+  filter(str_detect(clorofila_a_mg_l , "<") )
+# Pasos para Acomodar la base:
+# 1. Eliminar valores especiales "< LD" (criterio conservador, se asumen NA)
+
+# Excluyo las variables factores y fechas que le voy a llamar variables "id"
+
+id_variables <- c("nombre_programa","codigo_pto","departamento","fecha_muestra",
+"fecha_hora","id_muestra") 
+
+# Con las que deberian ser pero muchas son caracteres por la presencia de los "LD", 
+# las paso a numericas 
+# los warnings avisan que se sustiuyeron caracteres raros por NA
+
+rios_numericas <- RN_RU %>% dplyr::select(-c(all_of(id_variables))) %>% 
+        mutate_if (is.character, as.numeric) 
+
+glimpse(rios_numericas) # son numericas todas(dbl o int) efectivamente
+
+# Pego las numericas a los factores y fechas de la original
+data_rios <- RN_RU %>% dplyr::select(all_of(id_variables)) %>% 
+  bind_cols (rios_numericas )
+  
+# Elimino las aclaraciones de los limites LD y LC ya que no retuve esos valores en las originales
+
+data_rios <- data_rios %>%  dplyr::select ( !ends_with(c("_ld", "_lc"))) %>% 
+              mutate(fecha_muestra = ymd(fecha_muestra) )
+
+glimpse(data_rios)
+
+
+# BBDD 1. OANCompleto_tidy ------------------------------------------------
+
+#write_csv(data_rios, file = "2.Datos/tidyOAN_datacomplet.csv")
+
+
+# 2. Seleccionar sitios y fechas indicados en el articulo (primer parrafo de MyM y Tabla 1)
+
+data_articulo <- data_rios %>% filter (nombre_programa == "Río Negro" & fecha_muestra >= "2009-05-01" | 
+                                         nombre_programa == "Río Negro" & fecha_muestra <= "2018-11-30" |
+                                         nombre_programa == "Rio Uruguay" & fecha_muestra >= "2014-06-01"|
+                                         nombre_programa == "Rio Uruguay" & fecha_muestra <= "2018-11-30")
+
+
+# 3. Seleccionar variables usadas en el articulos
+
+# Variables usada en Bereta
+bereta_variables <- c("clorofila_a_mg_l", "alc_t_mg_ca_co3_l", "conduc_m_s_cm", "pt_mg_p_l",
+                      "sst_mg_l", "p_h_sin_unid","t_º_c")
+
+
+data_articulo <- data_articulo %>% dplyr::select(all_of(id_variables),all_of(bereta_variables))
+
+# BBDD 2. Data articulo ------------------------------------------------
+
+#write_csv(data_articulo, file = "2.Datos/data_articulo.csv")
+
+
+# Umbral del 99.5 de clo a. ¿Para todos los rios o por rio? 
+bereta_out1 <- data_articulo %>%  summarize (Q99.5 = quantile(clorofila_a_mg_l, probs = c(0.995),na.rm=T))
+
+data_articulo %>% filter (clorofila_a_mg_l >= bereta_out1$Q99.5   ) %>% 
+  select(codigo_pto, fecha_muestra, clorofila_a_mg_l)
+
+# Me quedan cuatro puntos. En articulo son 5. QUe pasa con RN12?
+data_articulo %>% filter (codigo_pto == "RN12" & fecha_muestra == "2018-04-17") %>% 
+  dplyr::select(codigo_pto, fecha_muestra, clorofila_a_mg_l)
+
+
+# Si calculo el 99.5 por rio... como el RU es muy bajo, me quedan muchos extremos!
+
+bereta_out2 <- data_articulo %>% group_by(nombre_programa) %>% 
+  summarize (Q99.5 = quantile(clorofila_a_mg_l, probs = c(0.995),na.rm=T))
+
+data_articulo %>% filter (clorofila_a_mg_l >= bereta_out2$Q99.5[[1]]   |
+                            clorofila_a_mg_l >= bereta_out2$Q99.5[[2]]) %>% 
+  select(codigo_pto, fecha_muestra, clorofila_a_mg_l)
+
+
+data_articulo %>% filter (!(codigo_pto == "RN12" & fecha_muestra == "2018-04-17"| 
+                            codigo_pto == "RN5" & fecha_muestra == "2012-01-19" |
+                            codigo_pto == "RN5" & fecha_muestra == "2012-12-06" |
+                            codigo_pto == "RN6" & fecha_muestra == "2012-08-15" |
+                            codigo_pto == "RN3" & fecha_muestra == "2012-12-05"  ))   
+
+# BBDD 3. Beretta data (99.5 out) -----------------------------------------
+
+data_bereta <-data_articulo %>% filter (!(codigo_pto == "RN12" & fecha_muestra == "2018-04-17"| 
+                                            codigo_pto == "RN5" & fecha_muestra == "2012-01-19" |
+                                            codigo_pto == "RN5" & fecha_muestra == "2012-12-06" |
+                                            codigo_pto == "RN6" & fecha_muestra == "2012-08-15" |
+                                            codigo_pto == "RN3" & fecha_muestra == "2012-12-05"  ))   
+
+#write_csv(data_bereta, file = "2.Datos/data_bereta.csv")
