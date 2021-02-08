94d72becd1d6c82b0b9514bb3a2341474591e22e
NAlcan <nalcann@gmail.com>
2021-02-08 19:00:55 -0300

    Avanzado en el codigo con los glm
    NO COINCIDEN los grados de libertad con el artiuclo y por ende los estimados

--- 1.Codigo/Resultados.R
+++ 1.Codigo/Resultados.R
@@ -8,10 +8,21 @@ data_articulo <- read_csv("2.Datos/data_articulo.csv")
 
 glimpse(data_articulo)
 
-id_variables <- c("nombre_programa","codigo_pto","departamento","fecha_muestra",
-                  "fecha_hora","id_muestra") 
+# Elimino el Cuareim que no se usa en este analisis
+# Edito los nombres de los programas del RN y RU
+# Y genero las variable Year y Mes
+
+data_articulo <- data_articulo %>% filter (nombre_programa != ("Rio Cuareim")) %>% 
+    mutate (nombre_programa = fct_recode(nombre_programa, 
+                                         RN = "Río Negro",
+                                         RU = "Rio Uruguay"),
+      Year = year(fecha_muestra),
+          Mes = month (fecha_muestra))
+
+
 # Umbral del 99.5 de clo a. ¿Para todos los rios o por rio? 
-bereta_out1 <- data_articulo %>%  summarize (Q99.5 = quantile(Clorofila_a, probs = c(0.995),na.rm=T))
+bereta_out1 <- data_articulo %>%
+  summarize (Q99.5 = quantile(Clorofila_a, probs = c(0.995),na.rm=T))
 
 data_articulo %>% filter (Clorofila_a >= bereta_out1$Q99.5   ) %>% 
   select(codigo_pto, fecha_muestra, Clorofila_a)
@@ -29,7 +40,8 @@ data_articulo <- data_articulo %>%
 id_variables <- c("nombre_programa","codigo_pto","departamento","fecha_muestra",
                   "fecha_hora","id_muestra","BeretOut") 
 
-Data_out <- ggplot(data_articulo,aes(y=Clorofila_a, x=nombre_programa)) +   geom_jitter(aes(color = BeretOut)) + 
+Data_out <- ggplot(data_articulo,aes(y=Clorofila_a, x=nombre_programa)) + 
+  geom_jitter(aes(color = BeretOut)) + 
   theme_minimal() + labs(x=NULL)
   
 ggplot(data_articulo, aes(y=Clorofila_a, x = FosforoTotal)) +
@@ -45,13 +57,22 @@ Plot_pred <- data_articulo %>%
   scale_color_manual(values = c("#d95f02","#1b9e77")) +
   facet_wrap( nombre_programa ~ xvar , scales="free") 
 
-Plot_descr <- data_articulo %>%
+Plot_descr_inn <- data_articulo %>%
   filter (BeretOut == "inn") %>% 
     pivot_longer(-c(all_of(id_variables), Clorofila_a),
                names_to="xvar",values_to="value") %>%
-  ggplot(aes(x = value, y = Clorofila_a)) + geom_point(aes(color= nombre_programa),alpha = 0.5) +
+  ggplot(aes(x = value, y = Clorofila_a)) + geom_point(alpha = 0.5) +
   scale_color_manual(values = c("#d95f02","#1b9e77")) +
-  facet_wrap( ~ xvar , scales="free") 
+  facet_grid(nombre_programa ~ xvar , scales="free") 
+
+Plot_descr_out <- data_articulo %>%
+#  filter (BeretOut == "inn") %>% 
+  pivot_longer(-c(all_of(id_variables), Clorofila_a),
+               names_to="xvar",values_to="value") %>%
+  ggplot(aes(x = value, y = Clorofila_a)) + geom_point(alpha = 0.5) +
+  scale_color_manual(values = c("#d95f02","#1b9e77")) +
+  facet_grid(nombre_programa ~ xvar , scales="free") 
+
 
  
 # Dato extremo de Temp
@@ -68,6 +89,31 @@ data_articulo %>%  group_by (nombre_programa) %>%
   summarize ( mediapH = mean (Ph, na.rm=T),
               meanTemp = mean(TempAgua, na.rm=T),
               meanPT = mean (FosforoTotal , na.rm = T))
+
+
+# GLM ---------------------------------------------------------------------
+
+ph_rn <- data_articulo %>% 
+  filter (BeretOut == "inn" & nombre_programa == "RN") %>% 
+  dplyr::select(Ph, codigo_pto, Year, Mes )
+  
+ph_rn_model <- lme4::lmer(Ph ~ Year  + (1 | codigo_pto/Mes), data = ph_rn )
+
+summary(ph_rn_model)
+
+#Alcalinidad 
+
+Alc_rn <- data_articulo %>% 
+  filter (BeretOut == "inn" & nombre_programa == "RN") %>% 
+  dplyr::select(Alcalinidad, codigo_pto, Year, Mes )
+
+Alc_rn_model <- lme4::lmer(Alcalinidad ~ Year  + (1 | codigo_pto/Mes), data = Alc_rn )
+
+summary(Alc_rn_model)
+
+
+# NO COINCIDEN LOS Grados de Libertad!!
+
 
 # Render RMarkdown ------------------------------------------------- 
 rmarkdown::render(
