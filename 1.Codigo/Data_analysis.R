53a864497d5c033d5e74fe294355016980875c15
NAlcan <nalcann@gmail.com>
2021-07-28 17:42:23 -0300

    Initilized Data Analysis code for publication

---
+++ 1.Codigo/Data_analysis.R
@@ -0,0 +1,8 @@
+# Function load -----------------------------------------------------
+library(tidyverse) # Data load/manipulation and graphics build
+library(RColorBrewer) # Color Palettes
+library(lubridate) # Day/Time Manipulation
+library(patchwork) # Plot Layout
+library(png) # Import `png` images
+
+theme_set(theme_minimal()) #  Graphic theme used for visualizations
