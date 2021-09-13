d3a655cba97003e9062f9eeb03bc8c91b641a578
NAlcan <nalcann@gmail.com>
2021-09-13 12:27:30 -0300

    Text formatted for publication

--- 1.Codigo/GIS_Data.R
+++ 1.Codigo/GIS_Data.R
@@ -8,56 +8,73 @@ library(snakecase) # Factor levels and names manipulation
 
 # Load sampling site coordinates table
 sites <- read_excel("2.Datos/gis_data/sites_references.xls",
-                     .name_repair = make_clean_names) %>% 
+                    .name_repair = make_clean_names) %>%
   mutate(
     programa = to_snake_case(programa),
     nombre_punto = to_snake_case (estacion),
-    .keep= "unused")
+    .keep = "unused"
+  )
 
 # OAN Database, to match the station used by
 oan_data <- read_csv("2.Datos/working_data/OAN_complet_data.csv")
 
 
 # Eliminate station from the bottom idetified by and `F` and the code end label
- sites <- sites %>% filter(!str_detect(codigo_estacion,"F"))
-   
- 
- # Filter by data points from oan data.
- sites_reply <- sites %>% 
-  filter(codigo_estacion %in% oan_data$estacion)
- 
+sites <- sites %>% filter(!str_detect(codigo_estacion, "F"))
 
- sites_reply <- sites_reply %>% 
-  mutate(type = ifelse(str_detect(codigo_estacion, c("RN","RU")), "train", 
-                       ifelse( str_detect(codigo_estacion, "RC"),"test","not_used"))) %>%
-  arrange(latitud, .desc = TRUE) %>% 
-   mutate(site_number = row_number()) # Number code for each site
+
+# Filter by data points from oan data.
+sites_reply <- sites %>%
+  filter(codigo_estacion %in% oan_data$estacion)
+
+
+sites_reply <- sites_reply %>%
+  mutate(type = ifelse(
+    str_detect(codigo_estacion, c("RN", "RU")),
+    "train",
+    ifelse(str_detect(codigo_estacion, "RC"), "test", "not_used")
+  )) %>%
+  arrange(latitud, .desc = TRUE) %>%
+  mutate(site_number = row_number()) # Number code for each site
 
 #write_csv(sites_reply, file = "2.Datos/gis_data/coordenadas_sitios.csv")
 
- 
- # Table A1 variables ------------------------------------------------------
- 
- # * `type` :  Negro or Uruguay = "train", Cuareim = "test", others = "not used"
- # * `basin` : Uruguay, Cuareim or SanSalvador  = "Uruguay", other = "negro"
- # *
- 
- # Calculates n data per site and starting end dates
-sites_oan_data <- oan_data %>% 
-   group_by(estacion) %>% 
-   summarize ( N_Station = length(estacion),
-               Start_Date = min(date),
-               End_Date = max(date)) %>% 
-   rename (Sampling_Point = "estacion")
- 
- 
- 
- table1a_data <-  sites_reply %>% 
-   rename (Sampling_Point = "codigo_estacion",
-           Latitude = "latitud",
-           Longitude = "longitud",
-           `Site Code` = "site_number") %>% 
-    mutate (Basin = ifelse(str_detect(Sampling_Point,c("RC","RU","SSS")), "Uruguay","Negro")) %>% 
-     full_join(sites_oan_data, by = "Sampling_Point") %>% 
-      dplyr::select(Basin,Sampling_Point,Latitude,Longitude,type,N_Station,Start_Date,End_Date, `Site Code`)  
- 
+
+# Table A1 variables ------------------------------------------------------
+
+# * `type` :  Negro or Uruguay = "train", Cuareim = "test", others = "not used"
+# * `basin` : Uruguay, Cuareim or SanSalvador  = "Uruguay", other = "negro"
+# *
+
+# Calculates n data per site and starting end dates
+sites_oan_data <- oan_data %>%
+  group_by(estacion) %>%
+  summarize (
+    N_Station = length(estacion),
+    Start_Date = min(date),
+    End_Date = max(date)
+  ) %>%
+  rename (Sampling_Point = "estacion")
+
+
+
+table1a_data <-  sites_reply %>%
+  rename (
+    Sampling_Point = "codigo_estacion",
+    Latitude = "latitud",
+    Longitude = "longitud",
+    `Site Code` = "site_number"
+  ) %>%
+  mutate (Basin = ifelse(str_detect(Sampling_Point, c("RC", "RU", "SSS")), "Uruguay", "Negro")) %>%
+  full_join(sites_oan_data, by = "Sampling_Point") %>%
+  dplyr::select(
+    Basin,
+    Sampling_Point,
+    Latitude,
+    Longitude,
+    type,
+    N_Station,
+    Start_Date,
+    End_Date,
+    `Site Code`
+  )
--- 1.Codigo/Mapa.R
+++ 1.Codigo/Mapa.R
@@ -1,7 +1,7 @@
 ## Libraries call ----
 library(tidyverse) # arreglar datos (dplyr) y graficar (ggplot2)
 library(sf) # simple features para trabajar con datos espaciales
-library(mapsf) # helps to design various cartographic representations 
+library(mapsf) # helps to design various cartographic representations
 library(geouy) # easily access official spatial data sets of Uruguay
 library(ggspatial)
 library(viridis) # color palette
@@ -16,98 +16,123 @@ library(ggsflabel)
 sites <- read_csv("2.Datos/gis_data/coordenadas_sitios.csv")
 
 # Convert it to spacial object with crs projection
-sites_longlat <- sites %>% 
-  st_as_sf (coords = c("longitud","latitud")) %>% 
+sites_longlat <- sites %>%
+  st_as_sf (coords = c("longitud", "latitud")) %>%
   st_sf (crs = 4326)
 
 
 # Load Uruguay border catrography from package `geouy`
 borders <- load_geouy("Uruguay")
- 
-rivers <- load_geouy("Cursos de agua navegables y flotables") %>% # Rivers cartography 
+
+rivers <-
+  load_geouy("Cursos de agua navegables y flotables") %>% # Rivers cartography
   st_cast("MULTILINESTRING") # Convert multicurve to multistring https://github.com/r-spatial/sf/issues/1194
 
 # Selected rivers and streams to plot
 # A vector with river names separated by conditional symbol "|"
-this_rivers <- paste("uruguay",
-                       "cuareim",
-                       "río_negro",
-                       "tacuarembó",
-                       "tacuarembó_chico",
-                       "de_la_plata",
-                       "san_salvador", sep = "|")
+this_rivers <- paste(
+  "uruguay",
+  "cuareim",
+  "río_negro",
+  "tacuarembó",
+  "tacuarembó_chico",
+  "de_la_plata",
+  "san_salvador",
+  sep = "|"
+)
 
 #
 study_rivers <- rivers %>%
-  mutate(nombre = to_snake_case(nombre)) %>% 
- filter(str_detect( nombre, this_rivers))
+  mutate(nombre = to_snake_case(nombre)) %>%
+  filter(str_detect(nombre, this_rivers))
 
 # Reservoirs and lagoons cartography dowloaded from DIva-Gis Free Spatial Data at country level `http://diva-gis.org/gdata`
-water_bodies <- st_read ("2.Datos/gis_data/ury_inlandwater") %>%  # Uruguay/InlandWater
-mutate (name = to_snake_case(NAME),
-        .keep = "unused") %>%  # te drop de old NAME column and retain the new one
-  filter(name %in% c("rio_uruguay_uruguay","embalse_del_rio_negro"))
+water_bodies <-
+  st_read ("2.Datos/gis_data/ury_inlandwater") %>%  # Uruguay/InlandWater
+  mutate (name = to_snake_case(NAME),
+          .keep = "unused") %>%  # te drop de old NAME column and retain the new one
+  filter(name %in% c("rio_uruguay_uruguay", "embalse_del_rio_negro"))
 
 # base map
 base_map <- ggplot() +
   geom_sf (data = borders, fill = "NA", color = "grey40") +
-  geom_sf (data = study_rivers, color = "#1f78b4") + 
+  geom_sf (data = study_rivers, color = "#1f78b4") +
   geom_sf (data = water_bodies, color = "#1f78b4") +
   theme_bw() +
   theme(panel.grid = element_blank())
 
 # Edit Rivers names to add a plot maintaining its x,y coorde
 
-river_labels <- study_rivers %>% 
-   mutate(name = fct_recode(nombre,
-                           `Tacuarembó Chico` = "arroyo_tacuarembó_chico",
-                           `Negro River` = "río_negro",
-                           `Uruguay River` = "río_uruguay",
-                           `Cuareim River` = "río_cuareim",
-                           `San Salvador` = "río_san_salvador",
-                           `Tacuarembó River` = "río_tacuarembó",
-                           `Río de la Plata` = "río_de_la_plata"))
+river_labels <- study_rivers %>%
+  mutate(
+    name = fct_recode(
+      nombre,
+      `Tacuarembó Chico` = "arroyo_tacuarembó_chico",
+      `Negro River` = "río_negro",
+      `Uruguay River` = "río_uruguay",
+      `Cuareim River` = "río_cuareim",
+      `San Salvador` = "río_san_salvador",
+      `Tacuarembó River` = "río_tacuarembó",
+      `Río de la Plata` = "río_de_la_plata"
+    )
+  )
 
 # Map with site points filled if are used or not into BC2011
 
 mapa_listo <-  base_map +
-  geom_sf(data = sites_longlat, aes(fill = type, shape = type),size = 1.5,
-          color= "black") +
+  geom_sf(
+    data = sites_longlat,
+    aes(fill = type, shape = type),
+    size = 1.5,
+    color = "black"
+  ) +
   scale_fill_viridis(option = "magma", discrete = T) +
-  scale_shape_manual(values = c(21,22,23),guide ="legend") + 
-  labs (fill = "Point Status", shape = "Point Status") + 
-  annotation_scale(location = "br", width_hint = 0.2,
+  scale_shape_manual(values = c(21, 22, 23), guide = "legend") +
+  labs (fill = "Point Status", shape = "Point Status") +
+  annotation_scale(location = "br",
+                   width_hint = 0.2,
                    text_cex = 0.5) +
-  annotation_north_arrow(location = "bl", which_north = "true", 
-                         pad_x = unit(2.2, "in"), pad_y = unit(2.5, "in"),
-                         style = north_arrow_fancy_orienteering) +
-  labs (x = "longitud", y = "latitud") + 
-  geom_sf_text(data = river_labels,
-                     aes(label = name), 
-                     size = 3, 
-                     fontface = "bold",
-                     family = "serif",
-                     hjust = c(`Tacuarembó Chico` = .7,
-                                  `Negro River` =  .2,
-                                  `Uruguay River` = 1,
-                                  `Cuareim River` =  .3,
-                                  `San Salvador` = -.5,
-                                  `Tacuarembó River` = .3,
-                                  `Río de la Plata` = 0.8) ,
-                     vjust = c(`Tacuarembó Chico` = 1.8,
-                                 `Negro River` =  3,
-                                 `Uruguay River` = 1.7,
-                                 `Cuareim River` =  -1.2,
-                                 `San Salvador` = 0,
-                                 `Tacuarembó River` = -1.8,
-                                 `Río de la Plata` = 2),
-                     angle = c(`Tacuarembó Chico` = 310,
-                               `Negro River` =  0,
-                               `Uruguay River` = 260,
-                               `Cuareim River` =  320,
-                               `San Salvador` = 340,
-                               `Tacuarembó River` = 300,
-                               `Río de la Plata` = 0)
+  annotation_north_arrow(
+    location = "bl",
+    which_north = "true",
+    pad_x = unit(2.2, "in"),
+    pad_y = unit(2.5, "in"),
+    style = north_arrow_fancy_orienteering
+  ) +
+  labs (x = "longitud", y = "latitud") +
+  geom_sf_text(
+    data = river_labels,
+    aes(label = name),
+    size = 3,
+    fontface = "bold",
+    family = "serif",
+    hjust = c(
+      `Tacuarembó Chico` = .7,
+      `Negro River` =  .2,
+      `Uruguay River` = 1,
+      `Cuareim River` =  .3,
+      `San Salvador` = -.5,
+      `Tacuarembó River` = .3,
+      `Río de la Plata` = 0.8
+    ) ,
+    vjust = c(
+      `Tacuarembó Chico` = 1.8,
+      `Negro River` =  3,
+      `Uruguay River` = 1.7,
+      `Cuareim River` =  -1.2,
+      `San Salvador` = 0,
+      `Tacuarembó River` = -1.8,
+      `Río de la Plata` = 2
+    ),
+    angle = c(
+      `Tacuarembó Chico` = 310,
+      `Negro River` =  0,
+      `Uruguay River` = 260,
+      `Cuareim River` =  320,
+      `San Salvador` = 340,
+      `Tacuarembó River` = 300,
+      `Río de la Plata` = 0
+    )
   )
 
 
@@ -115,13 +140,19 @@ mapa_listo <-  base_map +
 
 # # Generate a character with equivalencies between number site and its code
 caption_sites <- paste(sites_longlat$site_number,
-                       sites_longlat$codigo_estacion, sep = " = ") %>%
+                       sites_longlat$codigo_estacion,
+                       sep = " = ") %>%
   str_c(., collapse = ", ")
 
-  
-vers1 <- mapa_listo + 
-  geom_sf_text_repel(data = sites_longlat, aes( label = site_number), 
-                     size = 2, max.overlaps = 20, box.padding = unit(0.1, "lines"))
+
+vers1 <- mapa_listo +
+  geom_sf_text_repel(
+    data = sites_longlat,
+    aes(label = site_number),
+    size = 2,
+    max.overlaps = 20,
+    box.padding = unit(0.1, "lines")
+  )
 
 # Save in png format
 #ggsave(vers1, filename = "3.Resultados/map_siterivers_png.png")
